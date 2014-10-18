26785cfb1ae6b8cb259b38c4245a66aee3fc534e
trinibago <rolle.christian@gmail.com>
2014-10-18 03:48:52 +0200

    Test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
-# TODO: tests, refactoring bar 
+# TODO: tests, refactoring foo bar 
 class Product < ActiveRecord::Base
   attr_accessible :category_id, :name
   validates_presence_of :category_id, :name
