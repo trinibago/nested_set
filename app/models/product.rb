368bf51a873231562256d550970adfaa7ea723ca
trinibago <rolle.christian@gmail.com>
2014-10-18 05:37:04 +0200

    Test really

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
-# TODO: tests, refactoring foo bartender is not cool
+# TODO: tests, refactoring foo bartender is not really cool
 class Product < ActiveRecord::Base
   attr_accessible :category_id, :name
   validates_presence_of :category_id, :name
