f2140fada4f7ff11571e66d656ff920591f22737
trinibago <rolle.christian@gmail.com>
2014-10-20 09:02:33 +0200

    Hottest fix ever

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
-# TODO: tests, refactoring foo bartender is cool
+# TODO: tests, refactoring foo bartender not is cool
 class Product < ActiveRecord::Base
   attr_accessible :category_id, :name
   validates_presence_of :category_id, :name
